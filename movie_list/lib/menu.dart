ffca793ec2894d6b84f2dedfcb2fdef7a2b10dd5
KenBalya <124903836+KenBalya@users.noreply.github.com>
2023-11-15 11:57:21 +0700

    commit

---
+++ movie_list/lib/menu.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:movie_list/widgets/left_drawer.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  final List<MovieItem> items = [
+    MovieItem("Lihat Item", Icons.checklist),
+    MovieItem("Tambah Item", Icons.add_shopping_cart),
+    MovieItem("Logout", Icons.logout),
+];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Movie List',
+        ),
+      ),
+      drawer: const LeftDrawer(),
+      body: SingleChildScrollView(
+        // Widget wrapper yang dapat discroll
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+          child: Column(
+            // Widget untuk menampilkan children secara vertikal
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                child: Text(
+                  'Welcome to Movie List V2!', // Text yang menandakan toko
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container pada card kita.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((MovieItem item) {
+                  // Iterasi untuk setiap item
+                  return MovieCard(item);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class MovieItem {
+  final String name;
+  final IconData icon;
+
+  MovieItem(this.name, this.icon);
+}
+
+class MovieCard extends StatelessWidget {
+  final MovieItem item;
+
+  const MovieCard(this.item, {super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Color.fromARGB(255, 181, 68, 191),
+      child: InkWell(
+        // Area responsive terhadap sentuhan
+        onTap: () {
+          // Memunculkan SnackBar ketika diklik
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Container(
+          // Container untuk menyimpan Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ movie_list/lib/movie_form.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:movie_list/widgets/left_drawer.dart';
+
+class ShopFormPage extends StatefulWidget {
+    const ShopFormPage({super.key});
+
+    @override
+    State<ShopFormPage> createState() => _ShopFormPageState();
+}
+
+class _ShopFormPageState extends State<ShopFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = "";
+    int _price = 0;
+    String _description = "";
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+  appBar: AppBar(
+    title: const Center(
+      child: Text(
+        'Form Tambah Produk',
+      ),
+    ),
+    backgroundColor: Colors.indigo,
+    foregroundColor: Colors.white,
+  ),
+  // TODO: Tambahkan drawer yang sudah dibuat di sini
+  drawer: const LeftDrawer(),
+
+  body: Form(
+    key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: "Nama Produk",
+              labelText: "Nama Produk",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+            onChanged: (String? value) {
+              setState(() {
+                _name = value!;
+              });
+            },
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return "Nama tidak boleh kosong!";
+              }
+              return null;
+            },
+          ),
+        ),
+      ]
+          
+        )
+      ),
+  ),
+);
+    }
+}
