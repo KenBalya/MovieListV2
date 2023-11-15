23793e0ad36d2f73d4c9678825d1f32fa7c7ac65
KenBalya <124903836+KenBalya@users.noreply.github.com>
2023-11-15 12:00:22 +0700

    Update movie_form.dart

--- movie_list/lib/movie_form.dart
+++ movie_list/lib/movie_form.dart
@@ -10,7 +10,7 @@ class ShopFormPage extends StatefulWidget {
 
 class _ShopFormPageState extends State<ShopFormPage> {
   final _formKey = GlobalKey<FormState>();
-  String _name = "";
+    String _name = "";
     int _price = 0;
     String _description = "";
     @override
@@ -57,6 +57,57 @@ class _ShopFormPageState extends State<ShopFormPage> {
             },
           ),
         ),
+        Padding(
+  padding: const EdgeInsets.all(8.0),
+  child: TextFormField(
+    decoration: InputDecoration(
+      hintText: "Harga",
+      labelText: "Harga",
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+    ),
+    // TODO: Tambahkan variabel yang sesuai
+    onChanged: (String? value) {
+      setState(() {
+        _price = int.parse(value!);
+      });
+    },
+    validator: (String? value) {
+      if (value == null || value.isEmpty) {
+        return "Harga tidak boleh kosong!";
+      }
+      if (int.tryParse(value) == null) {
+        return "Harga harus berupa angka!";
+      }
+      return null;
+    },
+  ),
+),
+Padding(
+  padding: const EdgeInsets.all(8.0),
+  child: TextFormField(
+    decoration: InputDecoration(
+      hintText: "Deskripsi",
+      labelText: "Deskripsi",
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+    ),
+    onChanged: (String? value) {
+      setState(() {
+        // TODO: Tambahkan variabel yang sesuai
+        _description = value!;
+      });
+    },
+    validator: (String? value) {
+      if (value == null || value.isEmpty) {
+        return "Deskripsi tidak boleh kosong!";
+      }
+      return null;
+    },
+  ),
+),
       ]
           
         )
